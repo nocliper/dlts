798c8950b2318032d9541ceec84098c1a60fc762
nocliper <31480657+nocliper@users.noreply.github.com>
2021-03-08 01:11:32 +0300

    Ea and sigma errors added

--- functions/dlts_plot.py
+++ functions/dlts_plot.py
@@ -24,9 +24,11 @@ def dlts_plot(T, Time, C, T1, T2, n_windows, ax, Smooth):
 
     DLTS = np.asarray(DLTS)
 
+    c = cm.gnuplot(np.linspace(0, 0.9, n_windows))
+
     for i in range(n_windows):
         tau = (Time[T2[i]]-Time[T1[i]])/np.log(Time[T2[i]]/Time[T1[i]])
-        ax.plot(T, DLTS[i], label = r'$\tau = %.3f$ s'%(tau))
+        ax.plot(T, DLTS[i], c = c[i], label = r'$\tau = %.3f$ s'%(tau))
 
     ax.legend()
     ax.grid(True,ls="-")
--- functions/find_max.py
+++ functions/find_max.py
@@ -1,4 +1,4 @@
-def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_h, ax1, ax2):
+def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_e, ax1, ax2):
     '''Returns Ea and crossection of trap
     in temperature interval T_start -> T_stop'''
 
@@ -6,6 +6,8 @@ def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_h, a
     from matplotlib import cm
     import numpy as np
     from scipy.optimize import curve_fit
+
+    c = cm.gnuplot(np.linspace(0, 0.9, n_windows))
 
     start = 0
     stop  = 0
@@ -48,7 +50,7 @@ def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_h, a
         if not loc_max:
             loc_max = 0
         else:
-            ax1.plot(T[start:stop][loc_max[0]],DLTS[i][start:stop][loc_max[0]],'x')
+            ax1.plot(T[start:stop][loc_max[0]], DLTS[i][start:stop][loc_max[0]], 'x', c = c[i])
 
 
     imax = 0
@@ -66,20 +68,18 @@ def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_h, a
         return -a*x + b
 
     popt, pcov = curve_fit(func, 1/Tx, np.log(Sx)) # your data x, y to fit
-    #print(popt)
+    perr = np.sqrt(np.diag(pcov)) # a and b error
 
     T_average = (T_start + T_stop)/2
 
-    Error = popt[0]*(8.617*10**-5)*(-2)*(T_stop-T_start)/(1.15*T_average*(np.log10(T_start**2/(6*T_stop**2))))##Bridman p.22 eq. 1.15
-
     ax2.set_ylabel(r'$\ln{(\tau^{-1}\cdot T^{-2})}, s^{-1}\cdot K^{-2}$')
     ax2.set_xlabel(r'Temperature $1/T, K^{-1}$')
-    ax2.plot(1/Tx,np.log(Sx),'o', label='Original Data')
-    ax2.plot(1/Tx,popt[1]-popt[0]/Tx,'k-',label='Fitted line')
+    ax2.scatter(1/Tx, np.log(Sx), c = c, label='Original Data')
+    ax2.plot(1/Tx, popt[1]-popt[0]/Tx, 'k-', label='Fitted line')
     ax2.legend()
     ax2.grid()
 
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    ax2.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
     plt.tight_layout()
 
     from tabulate import tabulate
@@ -87,7 +87,14 @@ def find_max(T_start,T_stop, DLTS, T, Time, T1, T2, X, n_windows, Doping, A_h, a
     t = tabulate(Table.T, headers=['1000/T, K-1', 'e/T^2, s-1*K-2'])
     print(t)
 
+    def sci_notation(a, b):
+        a_pow = np.ceil(np.log10(a))-1
+        b_pow = np.ceil(np.log10(b))-1
+        return a/10**a_pow, b/10**a_pow, a_pow
+
+    sigm, serr, spow = sci_notation(np.exp(popt[1])/A_e, np.exp(perr[1])/A_e)
+
     from IPython.display import display, Math
-    display(Math(r'$$E_C - E_t = ({} \pm {})eV$$'.format(round(popt[0]*(8.617*10**-5),3),round(Error,3))))
+    display(Math(r'$$E_C - E_t = ({} \pm {}) eV$$'.format(round(popt[0]*(8.617*10**-5),3), round(perr[0]*(8.617*10**-5),3))))
     display(Math(r'$$N_t = %.2E cm^2$$'%N_t))
-    display(Math(r'$$\sigma_p = %.2E cm^2$$'%(np.exp(popt[1])/A_h)))
+    display(Math(r'$$\sigma_n = (%.3f \pm %.3f)\cdot {10}^{%.0f} {cm}^2$$'%(sigm, serr, spow)))
